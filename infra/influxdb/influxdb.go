a63c8d316a5119d1bc4d4ee47ea476441e5dc3d7
walnuts1018 <r.juglans.1018@gmail.com>
2023-09-18 20:02:27 +0900

    add

--- infra/influxdb/influxdb.go
+++ infra/influxdb/influxdb.go
@@ -1,6 +1,9 @@
 package influxdb
 
 import (
+	"context"
+	"time"
+
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
 	"github.com/influxdata/influxdb-client-go/v2/api"
 	"github.com/walnuts1018/fitbit-manager/config"
@@ -10,17 +13,29 @@ import (
 type client struct {
 	influxdbClient influxdb2.Client
 	queryAPI       api.QueryAPI
+	writeAPI       api.WriteAPIBlocking
 }
 
 func NewClient(endpoint string) domain.DataStore {
 	c := influxdb2.NewClient(endpoint, config.Config.InfluxDBAuthToken)
+	w := c.WriteAPIBlocking(config.Config.InfluxDBAuthToken, config.Config.InfluxDBBucket)
 	q := c.QueryAPI(config.Config.InfluxDBOrg)
-	return &client{
+	return client{
 		influxdbClient: c,
 		queryAPI:       q,
+		writeAPI:       w,
 	}
 }
 
-func (c *client) Close() {
+func (c client) Close() {
 	c.influxdbClient.Close()
+}
+
+func (c client) test() {
+	p := influxdb2.NewPointWithMeasurement("stat").
+		AddTag("unit", "temperature").
+		AddField("avg", 23.2).
+		AddField("max", 45.0).
+		SetTime(time.Now())
+	c.writeAPI.WritePoint(context.Background(), p)
 }
